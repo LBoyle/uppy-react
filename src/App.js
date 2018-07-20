83e72957ee60c9edd8a3651dee55d4c2a3e152ae
LBoyle <l.boyle@nittygritty.net>
2018-07-20 23:29:25 +0100

    moved reader and uppy into the component class

--- src/App.js
+++ src/App.js
@@ -7,12 +7,89 @@ const DragDrop = require('@uppy/drag-drop');
 const ProgressBar = require('@uppy/progress-bar');
 // const Tus = require('@uppy/tus');
 
-const uppyOne = new Uppy({ debug: true, autoProceed: false });
-const reader = new FileReader();
 
-uppyOne.on('complete', (result) => {
-  console.log(result);
-});
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uppyPreviews: []
+    };
+
+    this.uploadFile = this.uploadFile.bind(this);
+    this.addFileToState = this.addFileToState.bind(this);
+
+    this.uppyOne = new Uppy({ debug: true, autoProceed: false });
+    this.reader = new FileReader();
+  }
+
+  componentDidMount() {
+    const addFileToState = this.addFileToState;
+    const reader = this.reader;
+
+    this.uppyOne
+      .use(DragDrop, { target: '#UppyOne' })
+      // .use(Tus, { endpoint: '//master.tus.io/files/' })
+      .use(ProgressBar, { target: '#UppyOne-Progress', hideAfterFinish: false });
+
+    this.uppyOne.on('complete', (result) => {
+      console.log(result);
+    });
+
+    this.uppyOne.on('file-added', (file) => {
+      console.log('Added file', file);
+      reader.onload = (readerEvt) => addFileToState({ file, base64: readerEvt.target.result });
+      reader.readAsDataURL(file.data);
+    });
+  }
+
+  addFileToState({ file, base64 }) {
+    this.setState({ uppyPreviews: [{ file, base64 }, ...this.state.uppyPreviews] });
+  }
+
+  uploadFile() {
+    console.log(this.state.uppyPreviews);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Uppy</h1>
+        </header>
+        <div className="UppyContainer">
+          <div id="UppyOne" />
+          <div id="UppyOne-Progress" />
+          <button onClick={this.uploadFile}>Log previews to console / Submit</button>
+        </div>
+        <div className="ImagePreviewContainer">
+          {
+            this.state.uppyPreviews.map(
+              item => {
+                return (
+                  <object
+                    key={item.file.id}
+                    type={item.file.type}
+                    width="200px"
+                    height="auto"
+                    data={item.base64}
+                  >
+                    {item.file.name}
+                  </object>
+                );
+              }
+            )
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
 
 // type uppy_preview_t = {
 //   file: {
@@ -47,71 +124,3 @@ uppyOne.on('complete', (result) => {
 //   },
 //   base64: string,
 // };
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      uppyPreviews: []
-    };
-    this.uploadFile = this.uploadFile.bind(this);
-    this.addFileToState = this.addFileToState.bind(this);
-  }
-  componentDidMount() {
-    const addFileToState = this.addFileToState;
-
-    uppyOne
-      .use(DragDrop, { target: '#UppyOne' })
-      // .use(Tus, { endpoint: '//master.tus.io/files/' })
-      .use(ProgressBar, { target: '#UppyOne-Progress', hideAfterFinish: false });
-
-    uppyOne.on('file-added', (file) => {
-      console.log('Added file', file);
-      reader.onload = (readerEvt) => addFileToState({ file, base64: readerEvt.target.result });
-      reader.readAsDataURL(file.data);
-    });
-
-  }
-  addFileToState({ file, base64 }) {
-    this.setState({ uppyPreviews: [{ file, base64 }, ...this.state.uppyPreviews] });
-  }
-  uploadFile() {
-    console.log(this.state.uppyPreviews);
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Uppy</h1>
-        </header>
-        <div className="UppyContainer">
-          <div id="UppyOne" />
-          <div id="UppyOne-Progress" />
-          <button onClick={this.uploadFile}>Upload</button>
-        </div>
-        <div className="ImagePreviewContainer">
-          {
-            this.state.uppyPreviews.map(
-              item => {
-                return (
-                  <object
-                    key={item.file.id}
-                    type={item.file.type}
-                    width="200px"
-                    height="auto"
-                    data={item.base64}
-                  >
-                    {item.file.name}
-                  </object>
-                );
-              }
-            )
-          }
-        </div>
-      </div>
-    );
-  }
-}
-
-export default App;
